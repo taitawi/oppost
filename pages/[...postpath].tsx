4fa72f228e1b984507497751e9ea7173fe0fb148
taitawi <137893829+taitawi@users.noreply.github.com>
2023-07-01 02:29:03 +0100

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -82,14 +82,7 @@ const Post: React.FC<PostProps> = (props) => {
 		<>
 			<Head>
 				
-				<link rel="canonical" href={`https://facebooke.com`} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
-				<meta property="og:url" content={`https://${host}/${path}`} />
-				<meta property="og:type" content="article" />
-				<meta property="og:locale" content="en_US" />
-				<meta property="og:site_name" content={host.split('.')[0]} />
-				<meta property="article:published_time" content={post.dateGmt} />
-				<meta property="article:modified_time" content={post.modifiedGmt} />
+
 				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
 				<meta
 					property="og:image:alt"
